b19c6618be7196095aa2523cf197d896b84d3060
alshatti92 <a.alshatti@hotmail.co.uk>
2022-10-31 19:06:51 +0300

    corrected the upcoming booking view to filter

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,8 @@
 from .models import Booking, Flight
 from rest_framework.generics import ListAPIView
 from .serializer import FlightListSerializer, BookingsListSerializer
+# to filter the time zone to show the coming booking only, u need to import timezone
+from django.utils import timezone
 
 
 class FlightsListViews(ListAPIView):
@@ -8,5 +10,5 @@ class FlightsListViews(ListAPIView):
     serializer_class = FlightListSerializer
 
 class BookingsListViews(ListAPIView):
-    queryset = Booking.objects.all()
+    queryset = Booking.objects.filter(date__gt=timezone.now)
     serializer_class = BookingsListSerializer
