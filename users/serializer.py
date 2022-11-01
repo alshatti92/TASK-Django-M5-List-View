f08564e83389757d7ff795c79e3c351fd1f95907
alshatti92 <a.alshatti@hotmail.co.uk>
2022-11-01 19:59:07 +0300

    task create , added first and last names

--- users/serializer.py
+++ users/serializer.py
@@ -6,7 +6,7 @@ class UserCreateSeriliazer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ["username", "password"]
+        fields = ["username", "password", "first_name", "last_name"]
 
     def create(self, validated_data):
         username = validated_data["username"]
