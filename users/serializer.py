473b757ec866d58f0f3d28b56cd13b03723377b1
alshatti92 <a.alshatti@hotmail.co.uk>
2022-11-02 18:35:33 +0300

    added validated_data and validated data pop

--- users/serializer.py
+++ users/serializer.py
@@ -1,3 +1,4 @@
+from re import T
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
@@ -9,11 +10,30 @@ class UserCreateSeriliazer(serializers.ModelSerializer):
         fields = ["username", "password", "first_name", "last_name"]
 
     def create(self, validated_data):
-        username = validated_data["username"]
-        password = validated_data["password"]
-        new_user = User(username=username)
+        # username = validated_data["username"]
+        password = validated_data.pop("password")
+        new_user = User(**validated_data)
         new_user.set_password(password)
         new_user.save()
         # it will return the validated data and will not show the password bcoz of the 1st comment.
         # we can return new_user in case we want to add a sentence where username = f"(sentence {username})"
-        return validated_data
+        return new_user
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        username = data.get("username")
+        password = data.get("password")
+
+        try:
+            username = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("if you forgot your username how you will remember your password !!")
+
+        if not username.check_password(password):
+            raise serializers.ValidationError("we didn't learn forgot password code, HAPPY ???")
+
+        return data
--- users/views.py
+++ users/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
-from .serializer import UserCreateSeriliazer
+from .serializer import UserCreateSeriliazer, UserLoginSerializer
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
 
 # Create your views here.
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserCreateSeriliazer
+
+
+class UserLoginAPIView(APIView):
+    serializer_class = UserLoginSerializer
+
+    def post(self, request):
+        data = request.data
+        serializer = UserLoginSerializer(data=data)
+
+        if serializer.is_valid(raise_exception=True):
+            valid_data = serializer.data
+            
